145cae15c424315eeec185e5802df402d412793c
Elena Magay <elenamagay1985@gmail.com>
2023-07-13 14:26:39 -0400

    middleware, controllers and routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,10 +3,28 @@ console.log('Express Tutorial')
 const express = require('express');
 const path = require('path');
 const { products } = require('./data');
-
+const { people } = require('./data');
 const app = express();
+const peopleRouter = require('./routes/people');
 
-app.use(express.static(path.join(__dirname, 'public')));
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const time = `${day}/${month}/${year}`;
+
+    console.log(method, url, time)
+    next();
+};
+app.use(logger);
+app.use(express.static(path.join(__dirname, './methods-public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api/v1/people', peopleRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
@@ -16,6 +34,7 @@ app.get('/api/v1/test', (req, res) => {
     res.json({message: 'It worked!'})
 });
 
+//products API
 app.get('/api/v1/products', (req, res) => {
     res.json(products)
 });
@@ -54,6 +73,20 @@ app.get("/api/v1/query", (req, res) => {
 
     return res.status(200).json(searchProducts)
 });
+
+//people API
+// app.get('/api/v1/people', (req, res) => {
+//     res.json(people)
+// });
+
+// app.post('/api/v1/people', (req, res) => {
+//     const { name } = req.body;
+//     if (!name) {
+//         res.status(400).json({ success: false, message: "Please provide a name" });
+//     }
+//     people.push({ id: people.length, name: req.body.name });
+//     res.status(201).json({ success: true, name: req.body.name });
+// })
 
 app.all('*', (req, res) => {
     res.status(404).send('Page not found')
