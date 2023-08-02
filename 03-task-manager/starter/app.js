29a83558674bfc006c15b2ea052fa60253327599
Elena Magay <elenamagay1985@gmail.com>
2023-08-02 07:15:36 -0400

    finishing Task Manager application

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,8 +5,11 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 //middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 //routes
@@ -16,9 +19,10 @@ app.get('/hello', (req,res) => {
 
 app.use('/api/v1/tasks', tasks);
 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
